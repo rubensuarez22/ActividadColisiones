d76756c28b695a3a0446c50f60e975995d5ac70f
rubensuarez22 <122560173+rubensuarez22@users.noreply.github.com>
2023-02-16 22:17:21 -0600

    Final

---
+++ Ball.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActividadColisiones
+{
+public class Ball
+{
+    public int X { get; set; }
+    public int Y { get; set; }
+    public int Radius { get; set; }
+    public int Vx { get; set; }
+    public int Vy { get; set; }
+    public Color Color { get; set; }
+
+        public Ball(int x, int y, int radius, int vx, int vy)
+    {
+        X = x;
+        Y = y;
+        Radius = radius;
+        Vx = vx;
+        Vy = vy;
+            Color = GetRandomColor();
+        }
+        private static Random random = new Random();
+        private static Color GetRandomColor()
+        {
+            return Color.FromArgb(random.Next(256), random.Next(256), random.Next(256));
+        }
+
+        public void Update(List<Ball> balls, int canvasWidth, int canvasHeight)
+        {
+            // Update the position of the ball
+            X += Vx;
+            Y += Vy;
+
+            // Check if the ball has hit a wall and reverse its velocity if it has
+            if (X - Radius < 0 || X + Radius > canvasWidth)
+            {
+                Vx = -Vx;
+            }
+            if (Y - Radius < 0 || Y + Radius > canvasHeight)
+            {
+                Vy = -Vy;
+            }
+
+            // Check for collisions with other balls
+            foreach (var ball in balls)
+            {
+                if (ball != this)
+                {
+                    int dx = X - ball.X;
+                    int dy = Y - ball.Y;
+                    int distance = (int)Math.Sqrt(dx * dx + dy * dy);
+                    int minDist = Radius + ball.Radius;
+
+                    if (distance < minDist)
+                    {
+                        // The balls have collided, so reverse their velocities
+                        int tx = Vx;
+                        int ty = Vy;
+                        Vx = ball.Vx;
+                        Vy = ball.Vy;
+                        ball.Vx = tx;
+                        ball.Vy = ty;
+                    }
+                }
+            }
+        }
+
+    }
+
+
+}
--- Form1.cs
+++ Form1.cs
@@ -1,54 +1,85 @@
-﻿using System;
+﻿using ActividadColisiones;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
 
 namespace ActividadColisiones
-{ 
-    public partial class Form1 : Form
 {
-
-    Bitmap bmp;
-    Graphics g;
-    Balls ball;
-    List<Balls> balls;
-    SolidBrush brush;
-    public Form1()
+    public partial class Form1 : Form
     {
-        InitializeComponent();
-    }
-    private void MAIN_Load(object sender, EventArgs e)
-    {
-        bmp = new Bitmap(PCT_CANVAS.Width, PCT_CANVAS.Height);
-        g = Graphics.FromImage(bmp);
-        PCT_CANVAS.Image = bmp;
-        balls = new List<Balls>();
+        Bitmap bmp;
+        Graphics g;
+        Ball ball;
+        List<Ball> balls;
+        SolidBrush brush;
 
-        for (int b = 0; b < 50; b++)
+        Timer timer = new Timer();
+
+        public Form1()
         {
-            balls.Add(new Balls(bmp.Size));
-        }
+            InitializeComponent();
 
+            bmp = new Bitmap(PCT_CANVAS.Width, PCT_CANVAS.Height);
+            g = Graphics.FromImage(bmp);
 
-    }
+            brush = new SolidBrush(Color.White);
 
-    private void TIMER_Tic(object sender, EventArgs e)
-        {
-            
-            g.Clear(Color.Black);
+            Random rand = new Random(); // declare and initialize the 'rand' variable
 
-            for (int b = 0; b < balls.Count; b++)
+            balls = new List<Ball>();
+            for (int i = 0; i < 10; i++)
             {
-                ball = balls[b];
-                g.FillEllipse(Brushes.Aqua, ball.p.X - (ball.size / 2), ball.p.Y - (ball.size / 2), ball.size, ball.size);
+                balls.Add(new Ball(rand.Next(PCT_CANVAS.Width), rand.Next(PCT_CANVAS.Height), rand.Next(10, 20), rand.Next(-5, 5), rand.Next(-5, 5)));
             }
 
-            PCT_CANVAS.Invalidate();
+            PCT_CANVAS.Paint += new PaintEventHandler(PCT_CANVAS_Paint);
+            timer.Interval = 10;
+            timer.Tick += new EventHandler(timer1_Tick);
+            timer.Start();
         }
+
+
+
+        private void PCT_CANVAS_Paint(object sender, PaintEventArgs e)
+        {
+         
+            e.Graphics.Clear(Color.Black);
+
+            foreach (var ball in balls)
+            {
+                using (SolidBrush brush = new SolidBrush(ball.Color)) { 
+                    e.Graphics.FillEllipse(brush, ball.X - ball.Radius, ball.Y - ball.Radius, ball.Radius * 2, ball.Radius * 2);
+                }
+            }
+        }
+
+
+
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+           
+            foreach (var ball in balls)
+            {
+                ball.Update(balls, PCT_CANVAS.Width, PCT_CANVAS.Height);
+            }
+
+            PCT_CANVAS.Refresh();
+        }
+
+
     }
 }
+
+
+
+
+
